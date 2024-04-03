055e0f06cd0ca5c3ef6ef5ec43da3b42c9603ed3
ptriantafy <pantrian04@gmail.com>
2024-04-03 15:48:44 +0300

    added repository annotation

--- src/main/java/gr/knowledge/internship/banksystem/repository/DocumentRepository.java
+++ src/main/java/gr/knowledge/internship/banksystem/repository/DocumentRepository.java
@@ -1,9 +1,11 @@
 package gr.knowledge.internship.banksystem.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import gr.knowledge.internship.banksystem.entity.Document;
 
+@Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
 
 }
