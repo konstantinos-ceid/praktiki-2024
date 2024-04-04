97f98a9f8a18a521856d4e2f01291dd67314c679
Xpecialist <manolouk2010@gmail.com>
2024-04-04 16:54:59 +0300

    delete something

--- src/main/java/gr/knowledge/internship/banksystem/repository/BankRepository.java
+++
@@ -1,9 +0,0 @@
-package gr.knowledge.internship.banksystem.repository;
-
-import gr.knowledge.internship.banksystem.entity.Bank;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BankRepository extends JpaRepository<Bank,Long> {
-}
--- src/main/java/gr/knowledge/internship/banksystem/repository/PhoneRepository.java
+++
@@ -1,9 +0,0 @@
-package gr.knowledge.internship.banksystem.repository;
-
-import gr.knowledge.internship.banksystem.entity.Phone;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PhoneRepository extends JpaRepository<Phone,Long> {
-}
