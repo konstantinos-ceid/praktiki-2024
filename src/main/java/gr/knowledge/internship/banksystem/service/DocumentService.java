9b3b500d33e9987c8a1e05e984c54b0690a0bcda
ptriantafy <pantrian04@gmail.com>
2024-04-04 10:53:30 +0300

    refactored exists in database to return Entity. changed getReferecyById to findById Added Javadocs

--- src/main/java/gr/knowledge/internship/banksystem/service/DocumentService.java
+++ src/main/java/gr/knowledge/internship/banksystem/service/DocumentService.java
@@ -8,58 +8,85 @@ import org.springframework.transaction.annotation.Transactional;
 
 import gr.knowledge.internship.banksystem.dto.DocumentDTO;
 import gr.knowledge.internship.banksystem.entity.Document;
+import gr.knowledge.internship.banksystem.mapper.DocumentMapper;
 import gr.knowledge.internship.banksystem.repository.DocumentRepository;
 import jakarta.persistence.EntityNotFoundException;
-import gr.knowledge.internship.banksystem.mapper.DocumentMapper;
 
-
+/**
+ * This class provides services for managing documents in the bank system.
+ */
 @Service
 @Transactional
 public class DocumentService {
+
 	@Autowired
 	private DocumentRepository documentRepository;
 	@Autowired
 	private DocumentMapper documentMapper;
 
+	/**
+	 * Retrieves a document by its ID.
+	 *
+	 * @param id The ID of the document to retrieve.
+	 * @return The DTO representation of the document.
+	 * @throws EntityNotFoundException If the document with the given ID is not
+	 *                                 found.
+	 */
 	@Transactional(readOnly = true)
 	public DocumentDTO getDocumentById(Long id) {
-		Document document = documentRepository.getReferenceById(id);
+		Document document = documentRepository.findById(id).orElseThrow(EntityNotFoundException::new);
 		return documentMapper.toDTO(document);
 	}
 
+	/**
+	 * Retrieves all documents.
+	 *
+	 * @return A list of DTO representations of all documents.
+	 */
 	@Transactional(readOnly = true)
 	public List<DocumentDTO> getAllDocuments() {
 		List<Document> allDocument = documentRepository.findAll();
 		return documentMapper.toDTOList(allDocument);
 	}
 
+	/**
+	 * Saves a new document.
+	 *
+	 * @param documentDTO The DTO representation of the document to be saved.
+	 * @return The DTO representation of the saved document.
+	 */
 	public DocumentDTO saveDocument(DocumentDTO documentDTO) {
-		Document documentToSave = documentMapper.toEntity(documentDTO);
-		documentRepository.save(documentToSave);
-		return documentMapper.toDTO(documentToSave);
+		Document document = documentMapper.toEntity(documentDTO);
+		documentRepository.save(document);
+		return documentMapper.toDTO(document);
 	}
 
+	/**
+	 * Updates an existing document.
+	 *
+	 * @param documentDTO The DTO representation of the document to be updated.
+	 * @return The DTO representation of the updated document.
+	 */
 	public DocumentDTO updateDocument(DocumentDTO documentDTO) {
-		if (this.existsInDatabase(documentDTO)) {
-			Document documentToSave = documentMapper.toEntity(documentDTO);
-			documentRepository.save(documentToSave);
-		}
+		Document document = this.existsInDatabase(documentDTO);
+		document = documentMapper.toEntity(documentDTO);
+		documentRepository.save(document);
 		return documentDTO;
 	}
 
-
+	/**
+	 * Deletes a document.
+	 *
+	 * @param documentDTO The DTO representation of the document to be deleted.
+	 */
 	public void deleteDocument(DocumentDTO documentDTO) {
-		Document documentToDelete = documentMapper.toEntity(documentDTO);
-		documentRepository.delete(documentToDelete);
+		Document document = documentMapper.toEntity(documentDTO);
+		documentRepository.delete(document);
 	}
 
-	private boolean existsInDatabase(DocumentDTO documentDTO) {
-		try {
-//			TODO global EntityNotFoundException handler
-			Document documentInDatabase = documentRepository.getReferenceById(documentDTO.getId());
-			return documentInDatabase != null;
-		} catch (NullPointerException npe) {
-			throw new EntityNotFoundException("Null document ID given.");
-		}
+	private Document existsInDatabase(DocumentDTO documentDTO) {
+		Document documentInDatabase = documentRepository.findById(documentDTO.getId())
+				.orElseThrow(EntityNotFoundException::new);
+		return documentInDatabase;
 	}
 }
--- src/main/java/gr/knowledge/internship/banksystem/service/FolderService.java
+++ src/main/java/gr/knowledge/internship/banksystem/service/FolderService.java
@@ -12,53 +12,80 @@ import gr.knowledge.internship.banksystem.mapper.FolderMapper;
 import gr.knowledge.internship.banksystem.repository.FolderRepository;
 import jakarta.persistence.EntityNotFoundException;
 
+/**
+ * This class provides services for managing folders in the bank system.
+ */
 @Service
 @Transactional
 public class FolderService {
+
 	@Autowired
 	private FolderRepository folderRepository;
 	@Autowired
 	private FolderMapper folderMapper;
 
+	/**
+	 * Retrieves a folder by its ID.
+	 *
+	 * @param id The ID of the folder to retrieve.
+	 * @return The DTO representation of the folder.
+	 * @throws EntityNotFoundException If the folder with the given ID is not found.
+	 */
 	@Transactional(readOnly = true)
 	public FolderDTO getFolderById(Long id) {
-		Folder folder = folderRepository.getReferenceById(id);
+		Folder folder = folderRepository.findById(id).orElseThrow(EntityNotFoundException::new);
 		return folderMapper.toDTO(folder);
 	}
 
+	/**
+	 * Retrieves all folders.
+	 *
+	 * @return A list of DTO representations of all folders.
+	 */
 	@Transactional(readOnly = true)
 	public List<FolderDTO> getAllFolders() {
 		List<Folder> allFolders = folderRepository.findAll();
 		return folderMapper.toDTOList(allFolders);
 	}
 
+	/**
+	 * Saves a new folder.
+	 *
+	 * @param folderDTO The DTO representation of the folder to be saved.
+	 * @return The DTO representation of the saved folder.
+	 */
 	public FolderDTO saveFolder(FolderDTO folderDTO) {
-		Folder folderToSave = folderMapper.toEntity(folderDTO);
-		folderRepository.save(folderToSave);
-		return folderMapper.toDTO(folderToSave);
+		Folder folder = folderMapper.toEntity(folderDTO);
+		folderRepository.save(folder);
+		return folderMapper.toDTO(folder);
 	}
 
+	/**
+	 * Updates an existing folder.
+	 *
+	 * @param folderDTO The DTO representation of the folder to be updated.
+	 * @return The DTO representation of the updated folder.
+	 */
 	public FolderDTO updateFolder(FolderDTO folderDTO) {
-		if(this.existsInDatabase(folderDTO)) {
-			Folder folderToSave = folderMapper.toEntity(folderDTO);
-			folderRepository.save(folderToSave);
-		}
+		Folder folder = this.existsInDatabase(folderDTO);
+		folder = folderMapper.toEntity(folderDTO);
+		folderRepository.save(folder);
 		return folderDTO;
 	}
 
-
+	/**
+	 * Deletes a folder.
+	 *
+	 * @param folderDTO The DTO representation of the folder to be deleted.
+	 */
 	public void deleteFolder(FolderDTO folderDTO) {
-		Folder folderToDelete = folderMapper.toEntity(folderDTO);
-		folderRepository.delete(folderToDelete);
+		Folder folder = folderMapper.toEntity(folderDTO);
+		folderRepository.delete(folder);
 	}
 
-	private boolean existsInDatabase(FolderDTO folderDTO) {
-		try {
-//			TODO global EntityNotFoundException handler
-			Folder folderInDatabase = folderRepository.getReferenceById(folderDTO.getId());
-			return folderInDatabase != null;
-		} catch (NullPointerException npe) {
-			throw new EntityNotFoundException("Null folder ID given.");
-		}
+	private Folder existsInDatabase(FolderDTO folderDTO) {
+		Folder folderInDatabase = folderRepository.findById(folderDTO.getId())
+				.orElseThrow(EntityNotFoundException::new);
+		return folderInDatabase;
 	}
 }
