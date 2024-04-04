dd8c7ea0bbf29e3eb70a6b22806eacc0ea88bb4f
Dionusia Psirri <66883669+Dionusia@users.noreply.github.com>
2024-04-04 14:59:44 +0300

    add path variable in update

--- src/main/java/gr/knowledge/internship/banksystem/service/BankRegistryService.java
+++ src/main/java/gr/knowledge/internship/banksystem/service/BankRegistryService.java
@@ -27,9 +27,38 @@ public class BankRegistryService {
         return modelMapper.map(companyList, new TypeToken<List<BankRegistryDTO>>(){}.getType());
     }
 
-    public BankRegistryDTO updateBankRegistry(BankRegistryDTO companyDTO){
-        bankRegistryRepository.save(modelMapper.map(companyDTO,BankRegistry.class));
-        return companyDTO;
+    /**
+     * Updates the details of a bank registry.
+     *
+     * @param id              The ID of the bank registry to be updated.
+     * @param bankRegistryDTO The updated bank registry data.
+     * @return The updated bank registry DTO.
+     * @throws IllegalArgumentException If the ID in the path variable does not match the ID in the request body.
+     * @throws RuntimeException         If the bank registry with the given ID is not found.
+     */
+    public BankRegistryDTO updateBankRegistry(Long id, BankRegistryDTO bankRegistryDTO) {
+        BankRegistry existingBankRegistry = findAndValidateIdMatch(id, bankRegistryDTO.getId());
+        existingBankRegistry.setName(bankRegistryDTO.getName());
+        bankRegistryRepository.save(existingBankRegistry);
+        return bankRegistryDTO;
+    }
+
+    /**
+     * Finds a bank registry by ID and validates if the ID in the path variable matches the ID in the request body.
+     *
+     * @param id            The ID of the bank registry to find.
+     * @param requestBodyId The ID present in the request body.
+     * @return The bank registry if found and the IDs match.
+     * @throws IllegalArgumentException If the ID in the path variable does not match the ID in the request body.
+     * @throws RuntimeException         If the bank registry with the given ID is not found.
+     */
+    private BankRegistry findAndValidateIdMatch(Long id, Long requestBodyId) {
+        BankRegistry existingBankRegistry = bankRegistryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Bank Registry not found with id: " + id));
+        if (!existingBankRegistry.getId().equals(requestBodyId)) {
+            throw new IllegalArgumentException("ID in path variable does not match ID in request body");
+        }
+        return existingBankRegistry;
     }
     public BankRegistryDTO saveBankRegistry(BankRegistryDTO bankRegistryDTO) {
         bankRegistryRepository.save(modelMapper.map(bankRegistryDTO, BankRegistry.class));
