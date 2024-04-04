d4957f1d3b28a28a8ad380695634d0d17c9fad22
Dionusia Psirri <66883669+Dionusia@users.noreply.github.com>
2024-04-04 14:48:37 +0300

    changes

--- src/main/java/gr/knowledge/internship/banksystem/entity/BankRegistry.java
+++ src/main/java/gr/knowledge/internship/banksystem/entity/BankRegistry.java
@@ -1,9 +1,11 @@
 package gr.knowledge.internship.banksystem.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Setter
@@ -11,55 +13,66 @@ import java.util.Date;
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 @Table(name = "bank_registry")
-public class BankRegistry {
+public class BankRegistry implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bank_registry_sequence")
     @SequenceGenerator(name = "bank_registry_sequence", sequenceName = "bank_registry_sequence", allocationSize = 50)
+    @NotNull
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name")
     @Size(max = 50)
+    @NotNull
     private String name;
 
-    @Column(name = "surname", nullable = false)
+    @Column(name = "surname")
     @Size(max = 50)
+    @NotNull
     private String surname;
 
-    @Column(name = "fathers_name", nullable = false)
+    @Column(name = "fathers_name")
     @Size(max = 50)
+    @NotNull
     private String fathersName;
 
-    @Column(name = "fathers_surname", nullable = false)
+    @Column(name = "fathers_surname")
     @Size(max = 50)
+    @NotNull
     private String fathersSurname;
 
-    @Column(name = "mothers_name", nullable = false)
+    @Column(name = "mothers_name")
     @Size(max = 50)
+    @NotNull
     private String mothersName;
 
-    @Column(name = "mothers_surname", nullable = false)
+    @Column(name = "mothers_surname")
     @Size(max = 50)
+    @NotNull
     private String mothersSurname;
 
-    @Column(name = "date_of_birth", nullable = false)
+    @Column(name = "date_of_birth")
+    @NotNull
     private Date dateOfBirth;
 
-    @Column(name = "place_of_birth", nullable = false)
+    @Column(name = "place_of_birth")
     @Size(max = 50)
+    @NotNull
     private String placeOfBirth;
 
-    @Column(name = "afm", nullable = false)
+    @Column(name = "afm")
     @Size(max = 20)
+    @NotNull
     private String afm;
 
-    @Column(name = "amka", nullable = false)
+    @Column(name = "amka")
     @Size(max = 20)
+    @NotNull
     private String amka;
 
-    @Column(name = "iban", nullable = false)
+    @Column(name = "iban")
     @Size(max = 50)
+    @NotNull
     private String iban;
 
     @ManyToOne
