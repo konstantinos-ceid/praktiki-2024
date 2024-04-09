ae236b5795b95f9683153e09e77a2224f0104f2e
ptriantafy <pantrian04@gmail.com>
2024-04-09 16:06:13 +0300

    added very basic logs with context

--- src/main/java/gr/knowledge/internship/banksystem/service/FolderService.java
+++ src/main/java/gr/knowledge/internship/banksystem/service/FolderService.java
@@ -2,6 +2,7 @@ package gr.knowledge.internship.banksystem.service;
 
 import java.util.List;
 
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,12 +12,14 @@ import gr.knowledge.internship.banksystem.entity.Folder;
 import gr.knowledge.internship.banksystem.mapper.FolderMapper;
 import gr.knowledge.internship.banksystem.repository.FolderRepository;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * This class provides services for managing folders in the bank system.
  */
 @Service
 @Transactional
+@Log4j2
 public class FolderService {
 
 	@Autowired
@@ -33,7 +36,10 @@ public class FolderService {
 	 */
 	@Transactional(readOnly = true)
 	public FolderDTO getFolderById(Long id) {
+		ThreadContext.put("folderId", id.toString());
 		Folder folder = folderRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+		log.debug("Request for folder with id");
+		ThreadContext.clearAll();
 		return folderMapper.toDTO(folder);
 	}
 
@@ -45,6 +51,9 @@ public class FolderService {
 	@Transactional(readOnly = true)
 	public List<FolderDTO> getAllFolders() {
 		List<Folder> allFolders = folderRepository.findAll();
+		ThreadContext.put("numberOfFolders", String.valueOf(allFolders.size()));
+		log.debug("Requested all folders");
+		ThreadContext.clearAll();
 		return folderMapper.toDTOList(allFolders);
 	}
 
@@ -55,8 +64,11 @@ public class FolderService {
 	 * @return The DTO representation of the saved folder.
 	 */
 	public FolderDTO saveFolder(FolderDTO folderDTO) {
+		ThreadContext.put("folderEntity", folderDTO.toString());
 		Folder folder = folderMapper.toEntity(folderDTO);
+		log.debug("Saving new folder");
 		folderRepository.save(folder);
+		ThreadContext.clearAll();
 		return folderMapper.toDTO(folder);
 	}
 
@@ -67,9 +79,11 @@ public class FolderService {
 	 * @return The DTO representation of the updated folder.
 	 */
 	public FolderDTO updateFolder(FolderDTO folderDTO) {
+		ThreadContext.put("folder", folderDTO.toString());
 		Folder folder = this.existsInDatabase(folderDTO);
 		folder = folderMapper.toEntity(folderDTO);
 		folderRepository.save(folder);
+		log.debug("Updating folder");
 		return folderDTO;
 	}
 
@@ -79,13 +93,18 @@ public class FolderService {
 	 * @param folderDTO The DTO representation of the folder to be deleted.
 	 */
 	public void deleteFolder(FolderDTO folderDTO) {
+		ThreadContext.put("folder", folderDTO.toString());
 		Folder folder = folderMapper.toEntity(folderDTO);
+		log.debug("Deleting folder");
+		ThreadContext.clearAll();
 		folderRepository.delete(folder);
 	}
 
 	private Folder existsInDatabase(FolderDTO folderDTO) {
 		Folder folderInDatabase = folderRepository.findById(folderDTO.getId())
 				.orElseThrow(EntityNotFoundException::new);
+		ThreadContext.put("existingFolder", folderDTO.toString());
+		log.debug("Folder exists in database");
 		return folderInDatabase;
 	}
 }
